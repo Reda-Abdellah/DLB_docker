2d9fda71289895ea4eda70b45933e4830939ab85
BorisMansencal <boris.mansencal@labri.fr>
2021-10-13 11:03:32 +0200

    several corrections
    - do not use /tmp anymore, but write directly in input image directory. [It allows to see that the pipeline is actually doing something when logged on the platform]
    - rename & remove files
    ...

--- end_to_end_pipeline_file.py
+++ end_to_end_pipeline_file.py
@@ -6,70 +6,124 @@ import shutil
 from report_utils import *
 from make_reports import *
 from utils import *
-import time  # B:TODO:DEBUG
+import time
+import glob  # B:TODO:DEBUG
 
 parser = argparse.ArgumentParser(
     description="""DeepLesionBrain platform version""", formatter_class=argparse.RawTextHelpFormatter)
 
-parser.add_argument('T1filename', type=str, help='T1 filename')
-parser.add_argument('T2filename', type=str, help='T2 filename')
+parser.add_argument('T1_filename', type=str, help='T1 filename')
+parser.add_argument('FLAIR_filename', type=str, help='FLAIR filename')
 parser.add_argument('--no_report', action='store_true')
 parser.add_argument('-sex', type=str, default='Unknown')
 parser.add_argument('-age', type=str, default='Unknown')
 args = parser.parse_args()
 
-nativeFLAIR_filename = os.path.join('/tmp', os.path.basename(args.T2filename).replace(".nii.gz", ".nii"))
-nativeT1_filename = os.path.join('/tmp', os.path.basename(args.T1filename).replace(".nii.gz", ".nii"))
 
-if('.gz' in args.T2filename):
-    shutil.copyfile(args.T2filename, nativeFLAIR_filename+'.gz')
-    os.system('gunzip '+nativeFLAIR_filename+'.gz')
-else:
-    shutil.copyfile(args.T2filename, nativeFLAIR_filename)
+print("args.T1_filename=", args.T1_filename)
+print("args.FLAIR_filename=", args.FLAIR_filename)
+tmp = sorted(glob.glob(os.path.join(os.path.dirname(args.T1_filename), "*")))
+print("####### files at the beginning: ")
+print(tmp)
+print("")
 
-if('.gz' in args.T1filename):
-    shutil.copyfile(args.T1filename, nativeT1_filename+'.gz')
-    os.system('gunzip '+nativeT1_filename+'.gz')
-else:
-    shutil.copyfile(args.T1filename, nativeT1_filename)
+native_t1_filename = args.T1_filename
+native_flair_filename = args.FLAIR_filename
+
+
+output_dir = os.path.dirname(args.T1_filename)
+
+tmp_t1_filename = os.path.join(os.path.dirname(native_t1_filename), os.path.basename(native_t1_filename).replace(".nii.gz", ".nii"))
+tmp_flair_filename = os.path.join(os.path.dirname(native_t1_filename), os.path.basename(native_flair_filename).replace(".nii.gz", ".nii"))
+
+if(native_t1_filename.endswith('.gz')):
+    assert(native_t1_filename != tmp_t1_filename)
+    run_command('gunzip -c {} > {}'.format(native_t1_filename, tmp_t1_filename))
+# else:
+#     shutil.copyfile(native_t1_filename, tmp_t1_filename)
+
+if(native_flair_filename.endswith('.gz')):
+    assert(native_flair_filename != tmp_flair_filename)
+    # shutil.copyfile(native_flair_filename, tmp_flair_filename+'.gz')
+    run_command('gunzip -c {} > {}'.format(native_flair_filename, tmp_flair_filename))
+# else:
+#     shutil.copyfile(native_flair_filename, tmp_flair_filename)
+
 
 t0 = time.time()
-mni_T1_filename, mni_flair_filename, mni_mask_filename, intot1, to_mni_affine, crisp_filename, hemi_fileneame, structures_filename = preprocess_file(nativeT1_filename, nativeFLAIR_filename, os.path.dirname(args.T1filename))
+mni_t1_filename, mni_flair_filename, mni_mask_filename, intot1, to_mni_affine, crisp_filename, hemi_fileneame, structures_filename = preprocess_file(tmp_t1_filename, tmp_flair_filename, output_dir)
+
+tmp = sorted(glob.glob(os.path.join(os.path.dirname(tmp_t1_filename), "*")))
+print("####### files {} after preprocess: ".format(os.path.dirname(tmp_t1_filename)))
+print(tmp)
+print("")
+
+
+# [platform] compress input files if necessary
+if(native_t1_filename.endswith('.gz')):
+    os.remove(tmp_t1_filename)
+    # run_command('gzip -9 -f {}'.format(native_t1_filename))
+    # native_t1_filename = native_t1_filename+'.gz'
+if(native_flair_filename.endswith('.gz')):
+    os.remove(tmp_flair_filename)
+    # run_command('gzip -9 -f {}'.format(native_flair_filename))
+    # native_flair_filename = native_flair_filename+'.gz'
+
+tmp = sorted(glob.glob(os.path.join(os.path.dirname(native_t1_filename), "*")))
+print("####### files {} after preprocess: ".format(os.path.dirname(native_t1_filename)))
+print(tmp)
+print("")
+
+
+
+
 t1 = time.time()
 Weights_list = keyword_toList(path='/Weights/', keyword='.h5')
 all_lesions_filename = segment_image(nbNN=[5, 5, 5], ps=[96, 96, 96],
                                      Weights_list=Weights_list,
-                                     T1=mni_T1_filename, FLAIR=mni_flair_filename,
+                                     T1=mni_t1_filename, FLAIR=mni_flair_filename,
                                      FG=mni_mask_filename, normalization="kde")
 t2 = time.time()
 
-native_mask = to_native(mni_mask_filename, to_mni_affine, nativeT1_filename, dtype='uint8')
-native_tissues = to_native(crisp_filename, to_mni_affine, nativeT1_filename, dtype='uint8')
-unfiltred_t1_filename = mni_T1_filename.replace('t1', 'unfiltred')
-to_MNI(nativeT1_filename, unfiltred_t1_filename, nativeT1_filename, mni_T1_filename)
+native_mask = to_native(mni_mask_filename, to_mni_affine, native_t1_filename, dtype='uint8')
+native_tissues = to_native(crisp_filename, to_mni_affine, native_t1_filename, dtype='uint8')
+# unfiltred_t1_filename = mni_t1_filename.replace('t1', 'unfiltred')
+# to_MNI(native_t1_filename, unfiltred_t1_filename, native_t1_filename, mni_t1_filename)
 t3 = time.time()
 
-os.system('gzip -f -9 '+mni_mask_filename)
-os.system('gzip -f -9 '+mni_flair_filename)
-os.system('gzip -f -9 '+mni_T1_filename)
-# os.system('gzip -f -9 '+mni_lesions_filename)
-os.system('gzip -f -9 '+nativeFLAIR_filename)
-os.system('gzip -f -9 '+nativeT1_filename)
-os.system('gzip -f -9 '+native_mask)
-os.system('gzip -f -9 '+native_tissues)
-os.system('gzip -f -9 '+crisp_filename)  # mni_tissues
-# os.system('gzip -f -9 '+hemi_fileneame)  # B:useless
-# os.system('gzip -f -9 '+structures_filename)  # B:useless
+tmp = sorted(glob.glob(os.path.join(os.path.dirname(mni_t1_filename), "*")))
+print("####### files here [before gzips] in {}: ".format(os.path.dirname(mni_t1_filename)))
+print(tmp)
+print("")
+
+
+run_command('gzip -f -9 '+mni_mask_filename)
+run_command('gzip -f -9 '+mni_flair_filename)
+run_command('gzip -f -9 '+mni_t1_filename)
+# run_command('gzip -f -9 '+mni_lesions_filename)
+# ##run_command('gzip -f -9 '+nativeFLAIR_filename)
+# ##run_command('gzip -f -9 '+native_t1_filename)
+run_command('gzip -f -9 '+native_mask)
+run_command('gzip -f -9 '+native_tissues)
+run_command('gzip -f -9 '+crisp_filename)  # mni_tissues
+# run_command('gzip -f -9 '+hemi_fileneame)  # B:useless
+# run_command('gzip -f -9 '+structures_filename)  # B:useless
 t4 = time.time()
 
-mni_lesion_filename = get_lesion_by_regions(mni_T1_filename+'.gz', crisp_filename+'.gz', hemi_fileneame, structures_filename, all_lesions_filename)
-# mni_lesion_filename is already gzipped (as passed T1 was)
+mni_lesion_filename = get_lesion_by_regions(mni_t1_filename+'.gz', crisp_filename+'.gz', hemi_fileneame, structures_filename, all_lesions_filename)
+# mni_lesion_filename is already gzipped (as passed mni_t1 was)
 t5 = time.time()
+
 print("mni_lesion_filename=", mni_lesion_filename)
-native_lesion = to_native(mni_lesion_filename, to_mni_affine, nativeT1_filename+'.gz', dtype='uint8')
-# os.system('gzip -f -9 '+mni_lesion_filename)
+tmp = sorted(glob.glob(os.path.join(os.path.dirname(mni_lesion_filename), "*")))
+print("####### files here in {}: ".format(os.path.dirname(mni_lesion_filename)))
+print(tmp)
+print("")
+
+native_lesion = to_native(mni_lesion_filename, to_mni_affine, native_t1_filename.replace(".nii.gz", ".nii")+'.gz', dtype='uint8')
+# run_command('gzip -f -9 '+mni_lesion_filename)
 print("native_lesion=", native_lesion)
-# os.system('gzip -f -9 '+native_lesion)
+# run_command('gzip -f -9 '+native_lesion)
 # B:TODO: comme on travaill sur des fichiers compressés, ça produit des fichiers compressés : mais on pourrait les compresser plus ????
 
 t6 = time.time()
@@ -77,26 +131,48 @@ t6 = time.time()
 if(not args.no_report):
     age = args.age.lower()
     sex = args.sex.lower()
-    report(unfiltred_t1_filename, mni_T1_filename+'.gz', mni_flair_filename+'.gz', mni_mask_filename+'.gz',  # all_lesions_filename+'.gz',
+    report(mni_t1_filename+'.gz', mni_flair_filename+'.gz', mni_mask_filename+'.gz',  # all_lesions_filename+'.gz',
            to_mni_affine, crisp_filename+'.gz', mni_lesion_filename, age, sex)
-os.remove(unfiltred_t1_filename)
+# os.remove(unfiltred_t1_filename)
 os.remove(hemi_fileneame)
 os.remove(structures_filename)
 os.remove(all_lesions_filename)
 
 t7 = time.time()
 
-# Save previews
+tmp = sorted(glob.glob(os.path.join(os.path.dirname(mni_lesion_filename), "*")))
+print("####### files here2 in {}: ".format(os.path.dirname(mni_lesion_filename)))
+print(tmp)
+print("")
+
+
+# Copy README.pdf
+shutil.copyfile("README.pdf", os.path.join(os.path.dirname(mni_t1_filename), "README.pdf"))
+
+# [platform] Save previews
 save_flair_preview(mni_flair_filename+'.gz')
 shutil.copyfile(mni_lesion_filename, os.path.join(os.path.dirname(mni_lesion_filename), "preview_"+os.path.basename(mni_lesion_filename)))
 shutil.copyfile(mni_mask_filename+".gz", os.path.join(os.path.dirname(mni_mask_filename), "preview_"+os.path.basename(mni_mask_filename)+".gz"))
+# [platform] Copy original files
+shutil.copyfile(native_t1_filename, os.path.join(os.path.dirname(mni_t1_filename), os.path.basename(mni_t1_filename).replace("mni_t1_", "original_t1_")+".gz"))
+shutil.copyfile(native_flair_filename, os.path.join(os.path.dirname(mni_t1_filename), os.path.basename(mni_t1_filename).replace("mni_t1_", "original_flair_")+".gz"))
+# remove original_t1_{}.nii (not compressed) produced by preprocessing
+# orig_t1_filename = os.path.join(os.path.dirname(mni_t1_filename), os.path.basename(mni_t1_filename).replace("mni_t1_", "original_t1_"))
+# os.remove(orig_t1_filename)
+
+tmp = sorted(glob.glob(os.path.join(os.path.dirname(mni_lesion_filename), "*")))
+print("####### files here3 in {}: ".format(os.path.dirname(mni_lesion_filename)))
+print(tmp)
+print("")
+
 
 t8 = time.time()
-print("time preprocess", (t1-t0))
-print("time segment", (t2-t1))
-print("time toNative", (t3-t2))
-print("time gzip", (t4-t3))
-print("time lesions", (t5-t4))
-print("time lesion native+gzip", (t6-t5))
-print("time report", (t7-t6))
-print("time previews", (t8-t7))
+print("time preprocess={:.2f}s".format(t1-t0))
+print("time segment={:.2f}s".format(t2-t1))
+print("time toNative={:.2f}s".format(t3-t2))
+print("time gzip={:.2f}s".format(t4-t3))
+print("time lesions={:.2f}s".format(t5-t4))
+print("time lesion native+gzip={:.2f}s".format(t6-t5))
+print("time report={:.2f}s".format(t7-t6))
+print("time previews={:.2f}s".format(t8-t7))
+print("TOTAL processing time={:.2f}s".format(t8-t0))
