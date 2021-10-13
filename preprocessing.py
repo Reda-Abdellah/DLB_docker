b57033b40b1460a4e715598263a50eded92721f4
BorisMansencal <boris.mansencal@labri.fr>
2021-10-13 10:59:00 +0200

    use run_command method, remove matlab log file, comment out unused method

--- preprocessing.py
+++ preprocessing.py
@@ -20,12 +20,12 @@ def preprocess_folder(in_folder_path, T1Keyword="T1", FLAIRKeyword='FLAIR'):  # 
         nativeFLAIR_name = nativeFLAIR_name.replace('.gz', '')
         if('.gz' in FLAIR):
             copyfile(FLAIR, nativeFLAIR_name + '.gz')
-            os.system('gunzip ' + nativeFLAIR_name + '.gz')
+            run_command('gunzip ' + nativeFLAIR_name + '.gz')
         else:
             copyfile(FLAIR, nativeFLAIR_name)
         if('.gz' in T1):
             copyfile(T1, nativeT1_name + '.gz')
-            os.system('gunzip ' + nativeT1_name + '.gz')
+            run_command('gunzip ' + nativeT1_name + '.gz')
         else:
             copyfile(T1, nativeT1_name)
         newT1, newFLAIR, new_mask, new_intot1, new_affine = preprocess_file(nativeT1_name, nativeFLAIR_name)
@@ -62,33 +62,12 @@ def preprocess_file(nativeT1_name, nativeFLAIR_name, output_dir):  # receives ab
     newHemi = os.path.join(output_dir, 'mni_hemi_' + T1_name)  # B:TODO:useless ???
     newStructures = os.path.join(output_dir, 'mni_structures_' + T1_name)  # B:TODO:useless ???
 
-    tmp = sorted(glob.glob(os.path.join(dirname, "*.nii*")))
-    print("####### files before preprocess: ")
-    print(tmp)
-
-    # newT1=nativeT1_name.replace('native_', 'mni_')
-    # newFLAIR=nativeFLAIR_name.replace('native_', 'mni_')
-    # outT1=nativeT1_name.replace('.nii', '_check.nii')
-    # outT1=outT1.replace('native_', 'n_mfmni_fnative_')
-    # out_intot1=nativeFLAIR_name.replace('.nii', '_checkAffine.txt')
-    # out_intot1=out_intot1.replace('native_', 'affine_intot1_fnative_')
-    # out_affine = nativeT1_name.replace('.nii', '_checkAffine.txt')
-    # out_affine = out_affine.replace('native_', 'affine_mfnative_')
-    # outFLAIR = outT1.replace('t1', 'flair')
-    # outMASK = outT1.replace('n_mfmni_', 'mask_n_mfmni_')
-    # out_crisp_filename = outT1.replace('n_mfmni_', 'crisp_mfmni_')
-    # out_hemi_fileneame = outT1.replace('n_mfmni_', 'hemi_n_mfmni_')
-    # out_structures_filename = outT1.replace('n_mfmni_', 'lab_n_mfmni_')
-    # new_crisp_filename = newT1.replace('mni_t1_', 'mni_tissues_')
-    # new_hemi_fileneame = newT1.replace('mni_t1_', 'mni_hemi_')
-    # new_structures_filename = newT1.replace('mni_t1_', 'mni_structure_')
-    # new_intot1 = nativeFLAIR_name.replace('native_', 'affine_intot1_fmni_').replace('.nii', '.txt')
-    # new_affine = nativeT1_name.replace('native_', 'affine_mfmni_').replace('.nii', '.txt')
 
     print('processing: ' + nativeT1_name + ' and ' + nativeFLAIR_name)
     bin = './lesionBrain_v11_fullpreprocessing_exe'
     command = bin + ' ' + nativeT1_name + ' ' + nativeFLAIR_name
-    os.system(command)
+    #run_command(command)
+    run_command(command)
 
     tmp = sorted(glob.glob(os.path.join(dirname, "*.nii*")))
     print("####### files after preprocess: ")
@@ -109,6 +88,8 @@ def preprocess_file(nativeT1_name, nativeFLAIR_name, output_dir):  # receives ab
     os.remove(nativeT1_name.replace('.nii', '_check.nii'))  # B:TODO: ???
     os.remove(nativeFLAIR_name.replace('.nii', '_check.nii'))
 
+    os.remove(os.path.join(dirname, "log.txt")) # matlab log file
+
     tmp = sorted(glob.glob(os.path.join(dirname, "*.nii*")))
     print("####### files after replace: ")
     print(tmp)
@@ -119,24 +100,24 @@ def preprocess_file(nativeT1_name, nativeFLAIR_name, output_dir):  # receives ab
     return newT1, newFLAIR, newMASK, newIntoT1, newAffine, newCrisp, newHemi, newStructures
 
 
-def ground_truth_toMNI(in_folder_path, preprocessed_out_folder, SEG_keyword):
-    ants_bin = './Registration/antsApplyTransforms'
-    for seg_keyword in SEG_keyword:
-        listaSEG = keyword_toList(in_folder_path, seg_keyword)
-        for inputname in listaSEG:
-            if('.gz' in inputname):
-                copyfile(inputname, 'tmp.nii.gz')
-                os.system('gunzip ' + 'tmp.nii.gz')
-                outputname = inputname.replace(seg_keyword, seg_keyword + '_MNI_')
-                outputname = outputname.replace('.gz', '')
-                command = ants_bin + ' -d 3 tmp.nii -r ' + reference_name + ' -o ' + outputname + ' -n MultiLabel[0.3,0] -t [' + transform_name + ', 1]'
-                os.system(command)
-                os.remove('tmp.nii')
-            else:
-                outputname = inputname.replace(seg_keyword, seg_keyword + '_MNI_')
-                command = ants_bin + ' -d 3 ' + inputname + ' -r ' + reference_name + ' -o ' + outputname + ' -n MultiLabel[0.3,0] -t [' + transform_name + ', 1]'
-                os.system(command)
-    files_list = keyword_toList(preprocessed_out_folder, '.')
-    for file in files_list:
-        if(not ('.gz' in file)):
-            os.system('gzip -f -9 '+file)
+# def ground_truth_toMNI(in_folder_path, preprocessed_out_folder, SEG_keyword):
+#     ants_bin = './Registration/antsApplyTransforms'
+#     for seg_keyword in SEG_keyword:
+#         listaSEG = keyword_toList(in_folder_path, seg_keyword)
+#         for inputname in listaSEG:
+#             if('.gz' in inputname):
+#                 copyfile(inputname, 'tmp.nii.gz')
+#                 run_command('gunzip ' + 'tmp.nii.gz')
+#                 outputname = inputname.replace(seg_keyword, seg_keyword + '_MNI_')
+#                 outputname = outputname.replace('.gz', '')
+#                 command = ants_bin + ' -d 3 tmp.nii -r ' + reference_name + ' -o ' + outputname + ' -n MultiLabel[0.3,0] -t [' + transform_name + ', 1]'
+#                 run_command(command)
+#                 os.remove('tmp.nii')
+#             else:
+#                 outputname = inputname.replace(seg_keyword, seg_keyword + '_MNI_')
+#                 command = ants_bin + ' -d 3 ' + inputname + ' -r ' + reference_name + ' -o ' + outputname + ' -n MultiLabel[0.3,0] -t [' + transform_name + ', 1]'
+#                 run_command(command)
+#     files_list = keyword_toList(preprocessed_out_folder, '.')
+#     for file in files_list:
+#         if(not ('.gz' in file)):
+#             run_command('gzip -f -9 '+file)
